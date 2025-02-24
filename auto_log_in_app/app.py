7cb34269df969f59f4983190df0e5376f9e8a228
Daniel C. <101406950+amseiden@users.noreply.github.com>
2025-02-24 22:41:26 +0200

    "Adjusted the timing"

---
+++ auto_log_in_app/app.py
@@ -0,0 +1,42 @@
+import time
+import pyautogui
+import pyperclip
+import subprocess
+
+
+def activate_application(app_name):
+    try:
+        subprocess.run(["open", "-a", app_name])
+        time.sleep(2)
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+def click_and_paste(x, y, text):
+    pyautogui.click(x, y)
+    time.sleep(0.2)
+    pyperclip.copy(text)
+    pyautogui.hotkey('command', 'v')
+    time.sleep(0.2)
+    pyautogui.press('enter')
+    time.sleep(2)
+
+
+def main():
+    app_name = "Cisco AnyConnect Secure Mobility Client"
+    activate_application(app_name)
+
+    time.sleep(2)
+
+    pyautogui.click(465, 180)
+    time.sleep(2)
+
+    click_and_paste(570, 290, "email")
+
+    click_and_paste(574, 347, "pass")
+
+    pyautogui.press('enter')
+
+
+if __name__ == "__main__":
+    main()
